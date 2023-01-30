5f252a8a9e2f745d766240d9cdda1cb0ecdf15c1
Scott Dark <47471022+ScottDark@users.noreply.github.com>
2023-01-30 01:34:45 -0500

    Updated and added functions

--- javascript.js
+++ javascript.js
@@ -6,12 +6,19 @@ const TIE = "You TIE this round";
 const LOSE = "You LOSE this round";
 const WIN = "You WIN this round";
 
+const TIE_GAME = "No Winners";
+const LOSE_GAME = "You Lose the game!";
+const WIN_GAME = "You Win the game!";
+
 let computerChoice;
-let setRoundCounter = 0;
+let setRoundCounter,
+  setWinCounter,
+  setLossCounter,
+  setTieCounter = 0;
 
 // Functions Start
 
-/* Returns: Either Rock, Paper, Scissors at random */
+/* Returns: Either Rock, Paper, Scissors at random - CPU */
 function getComputerChoice() {
   //Generates computer selection between 1-3
   computerChoice = Math.floor(Math.random() * 3) + 1;
@@ -38,48 +45,74 @@ function getRoundResult(playerChoice) {
     case ROCK:
       if (computerChoice === ROCK) {
         roundResult.textContent = TIE;
+        setTieCounter++;
         break;
       } else if (computerChoice === PAPER) {
         roundResult.textContent = LOSE;
+        setLossCounter++;
         break;
       } else {
         roundResult.textContent = WIN;
+        setWinCounter++;
         break;
       }
 
     case PAPER:
       if (computerChoice === PAPER) {
         roundResult.textContent = TIE;
+        setTieCounter++;
         break;
       } else if (computerChoice === ROCK) {
         roundResult.textContent = WIN;
+        setWinCounter++;
         break;
       } else {
         roundResult.textContent = LOSE;
+        setLossCounter++;
         break;
       }
 
     case SCISSORS:
       if (computerChoice === SCISSORS) {
         roundResult.textContent = TIE;
+        setTieCounter++;
         break;
       } else if (computerChoice === ROCK) {
         roundResult.textContent = LOSE;
+        setLossCounter++;
         break;
       } else {
         roundResult.textContent = WIN;
+        setWinCounter++;
         break;
       }
 
     default:
       break;
   }
+  setRoundCounter++;
 }
+
+/* Returns: Updated win/lose/tie result */
+function runningResults() {
+  const runningResult = document.querySelector(".runningResult");
+  runningResult.textContent = setWinCounter;
+  runningResult.textContent = setLossCounter;
+  runningResult.textContent = setTieCounter;
+}
+
+/* Returns: Result of game */
+function determineGameResult() {
+  if (setLossCounter === 3) {
+  }
+}
+
 /* Plays a round */
 function playRound() {
   const playerChoice = this.id;
   getComputerChoice();
   getRoundResult(playerChoice);
+  determineGameResult();
 }
 
 // Functions End
